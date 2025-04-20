d20d0b2df17711cc36a700c7ce186204bf350a65
Soc Basillote <dicecepe@gmail.com>
2025-04-20 20:31:30 +0800

    Adding health check path endpoint

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
+/// code
+app.get("/health", (req, res) => {
+
+  res.sendState(200);
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
